3c1f854d1fc0e45063dfe545ebc9789f038788c4
unknown <oalme004@fiu.edu>
2018-08-22 14:46:28 -0400

    initialized app router

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import {Routes, RouterModule, PreloadAllModules} from '@angular/router';
+
+const routes: Routes = [
+  {path: '', pathMatch: 'full', loadChildren: './home/home.module#HomeModule'},
+  {path: '**', redirectTo: ''}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {HomeModule} from './home/home.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'serverApp' })
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
