a760a8a1879f2de919cb67bd6fbdf6cbee30cb1f
unknown <oalme004@fiu.edu>
2018-08-22 17:10:37 -0400

    rest of feature modules with routing created, shared module created

---
+++ src/app/video/video.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { VideoRoutingModule } from './video-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    VideoRoutingModule
+  ],
+  declarations: []
+})
+export class VideoModule { }
