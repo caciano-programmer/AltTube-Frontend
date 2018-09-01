875dded530a2608fb93ac5095c3a2dea73594b14
unknown <oalme004@fiu.edu>
2018-08-31 21:09:12 -0400

    footer completed

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  template: `<nav class="navbar fixed-bottom"> Copyright© Cacinao Almeida. All rights reserved. </nav>`,
+  template: `<nav class="navbar fixed-bottom"> <span>Copyright© Cacinao Almeida.<span> All rights reserved.</span></span> </nav>`,
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {}
